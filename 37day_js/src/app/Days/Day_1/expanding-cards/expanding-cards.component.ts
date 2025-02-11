b49e8df40c94dfcfa938c42889c1f874791855ea
Tayyip Kose <tayyipkose037@gmail.com>
2025-02-11 09:52:26 +0300

    expanding-cards component created

---
+++ 37day_js/src/app/Days/Day_1/expanding-cards/expanding-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expanding-cards',
+  templateUrl: './expanding-cards.component.html',
+  styleUrls: ['./expanding-cards.component.css']
+})
+export class ExpandingCardsComponent {
+
+}
--- 37day_js/src/app/app.module.ts
+++ 37day_js/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpandingCardsComponent } from './Days/Day_1/expanding-cards/expanding-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpandingCardsComponent
   ],
   imports: [
     BrowserModule,
